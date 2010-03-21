6a80eddd9f966eef60b00dc9f66ed750af4f5195
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-03-21 14:41:36 +0000

    non-moose todo: this isn't called anymore

--- t/lib/Hailo/Test/Tokenizer.pm
+++ t/lib/Hailo/Test/Tokenizer.pm
@@ -1,12 +1,11 @@
 package Hailo::Test::Tokenizer;
 use 5.010;
-use Any::Moose;
-use namespace::clean -except => 'meta';
-use Hailo::Tokenizer::Words;
+use strict;
+use parent 'Hailo::Role::Tokenizer';
 
-with 'Hailo::Role::Tokenizer';
+die "XXX: This isn't called anymore!";
 
 sub make_tokens { goto &Hailo::Tokenizer::Words::make_tokens }
 sub make_output { goto &Hailo::Tokenizer::Words::make_output }
 
-__PACKAGE__->meta->make_immutable;
+1;
